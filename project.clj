defb8ceb40a0a8c07506a41ae602f643d9667a1f
Stefan Edlich <edlich@gmail.com>
2013-11-06 13:22:57 +0100

    Get JSON out of ArangoDB in ArrayMap format

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clarango "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.7"]
+                 [cheshire "5.2.0"]]
+  :main clarango.core) ;; for testing without a lib
---
+++ src/clarango/core.clj
@@ -0,0 +1,27 @@
+(ns clarango.core
+	(:require [clj-http.client :as http] ;; https://github.com/dakrone/clj-http
+		        [cheshire.core :refer :all]) ;; https://github.com/dakrone/cheshire for custom coding
+	(:use clojure.pprint))
+
+(declare READ) ;; just for testing with main, lib does not need this later
+
+;; maybe a universal defrecord is better like neocons does
+(def conn-adr "http://localhost:8529/_db/_system/_api/document/persons/16600192")
+
+(defn READ [url] ;; define timeout?
+	(parse-string (:body (http/get url))))
+
+;; --------------------------------------------
+;; DEMO to call lein run and test it without the lib usage
+(defn -main []
+	(let [result (READ conn-adr)]
+		(println "RAW:")
+		(pprint result)
+		(println "\nSTRIPPED:")
+		(pprint  (get result "001")))) ;; strip off the rest, select key to get the real content
+
+;; Test adresses:
+;; http://echo.jsontest.com/key/value/one/two
+;; http://localhost:8529/_db/_system/_api/document/persons/16600192
+
+;; {"001" {"age" 42, "name" "edlich"}, "_id" "persons/16600192", "_rev" "18238592",  "_key" "16600192"}
