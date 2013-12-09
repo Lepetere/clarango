56a25177f32a3f6447f87c70935c13fed5f889e2
Peter Fessel <peter.fessel@live.de>
2013-12-09 20:23:11 +0100

    suggestions for connect methods added to test.clj

--- src/clarango/test.clj
+++ src/clarango/test.clj
@@ -22,7 +22,32 @@
 		(println "\nRAW:")
 		(pprint result)
 		(println "\nSTRIPPED:")
-		(pprint  (get result "002")))) ;; strip off the rest, select key to get the real content
+		(pprint  (get result "002"))) ;; strip off the rest, select key to get the real content
+	
+	;; suggestion for connection interface
+	#_(
+	(let [connection (clarango.core/connect-host "http://localhost:8529/")]
+		(clarango.document/get connection "db_name" "collection_name" "document_id"))
+
+	(let [connection (clarango.core/connect-db "http://localhost:8529/" "db_name")]
+		(clarango.document/get connection "collection_name" "document_id"))
+
+	(let [connection (clarango.core/connect-collection "http://localhost:8529/" "db_name" "collection_name")]
+		(clarango.document/get connection "document_id"))
+
+	;; would be even cooler if the following also works:
+	(let [connection-host (clarango.core/connect-host "http://localhost:8529/")
+			connection-db (clarango.core/connect-db connection-host "db_name")
+			connection-collection (clarango.core/connect-collection connection-db "collection_name")]
+		(clarango.document/get connection-collection "document_id"))
+
+	;; even more connection methods to create a new db and collection
+	(let [connection (clarango.core/connect-new-db "http://localhost:8529/" "db_name")]
+		(clarango.collection/create connection "collection_name"))
+
+	(let [connection-db (clarango.core/connect-db "http://localhost:8529/" "db_name")
+			connection-collection (clarango.core/connect-new-collection connection-db "new_collection_name")] ;; problem here is: clarango.core/connect-new-collection cannot call clarango.collection/create due to dependency direction; that means we would have kind of a create collection method in both namespaces...
+		(clarango.document/post connection-collection {name: "Peter"}))))
 
 ;; Test adresses:
 ;; http://echo.jsontest.com/key/value/one/two
