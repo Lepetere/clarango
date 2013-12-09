5b434ea3dcf9bcac39ec4241cb5820140fa7599e
Peter Fessel <peter.fessel@live.de>
2013-12-09 13:13:34 +0100

    fix project meta data

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clarango "0.1.0-SNAPSHOT"
+(defproject clarango "0.0.2-SNAPSHOT"
   :description "A Clojure client for the HTTP API of ArangoDB"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/edlich/clarango"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.7"]
                  [cheshire "5.2.0"]]
-  :main clarango.main) ;; for testing without a lib
+  :main clarango.main)
