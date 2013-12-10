97210efa877c1a18a22d184af96398e2cbd5be5f
Peter Fessel <peter.fessel@live.de>
2013-12-10 20:11:33 +0100

    test.clj deleted; simple test is done in main now

--- src/clarango/main.clj
+++ src/clarango/main.clj
@@ -3,10 +3,23 @@
 		        [cheshire.core :refer :all]) ;; https://github.com/dakrone/cheshire for custom coding
 	(:require [clarango.core :as clarango.core]
 				[clarango.collection :as collection]
-				[clarango.document :as document]
-				[clarango.test :as test])
+				[clarango.document :as document])
 	(:use clojure.pprint))
 
+(def conn-path "_api/document/persons/23478695")
+
+;; DEMO to call lein run and test it without the lib usage
 (defn -main []
   (clarango.core/connect! {:connection-url  "http://localhost:8529/"})
-	(test/run-simple-test))
+  (let [result1 (document/get-key conn-path "001")
+    result2 (document/get-key conn-path "002")]
+      (pprint result1)
+      (pprint result2)))
+
+;; TODO next: 
+;; - document get method should determine if there is a global object set, overload it
+;; - put READ method to clarango.core?
+
+;; Test adresses:
+;; http://echo.jsontest.com/key/value/one/two
+;; http://localhost:8529/_db/_system/_api/document/persons/16600192
