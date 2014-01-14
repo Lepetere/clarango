7c64cc65f39b26fc5e19edeec03209d9e3c0820c
Peter Fessel <peter.fessel@live.de>
2014-01-14 11:29:28 +0100

    add graph dummy methods and extra namespaces for vertices and edges

--- src/clarango/graph.clj
+++ src/clarango/graph.clj
@@ -2,3 +2,22 @@
   (:require [clarango.core :as clarango.core]
   			[clarango.utilities.core-utility :as core-utility]
   			[clarango.utilities.graph-utility :as graph-utility]))
+
+(defn get-info
+  "Returns information about a graph."
+  []
+  nil)
+
+(defn create
+  "Creates a new graph."
+  []
+  nil)
+
+(defn delete
+  "Deletes a graph."
+  []
+  nil)
+
+(defn execute-traversal
+  "Sends a traversal to the server to execute.]
+  nil)
---
+++ src/clarango/vertex.clj
@@ -0,0 +1,25 @@
+(ns clarango.vertex
+  (:require [clarango.core :as clarango.core]
+            [clarango.utilities.core-utility :as core-utility]
+            [clarango.utilities.graph-utility :as graph-utility]
+            [clarango.utilities.http-utility :as http]))
+
+(defn get-by-key
+  "Gets a vertex by it's key."
+  [& args]
+  nil)
+
+(defn replace-by-key
+  "Replace a vertex by it's key."
+  [& args]
+  nil)
+
+(defn update-by-key
+  "Updates a vertex by it's key."
+  [& args]
+  nil)
+
+(defn delete-by-key
+  "Deletes a vertex by it's key."
+  [& args]
+  nil)
