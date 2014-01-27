e3fd90d5244692435b2700904e71abcf75ca32ba
Peter Fessel <peter.fessel@live.de>
2014-01-27 15:41:09 +0100

    use new clj-http version

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.7"]
+                 [clj-http "0.7.8"]
                  [cheshire "5.2.0"]]
   :main clarango.main)
