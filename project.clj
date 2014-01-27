38c046cd574dfd4e96d88e2169b26514e4f8056f
Peter Fessel <peter.fessel@live.de>
2014-01-27 15:24:16 +0100

    delete database-utility: not needed

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clarango "0.0.2-SNAPSHOT"
+(defproject clarango "0.1.0"
   :description "A Clojure client for the HTTP API of ArangoDB"
   :url "https://github.com/edlich/clarango"
   :license {:name "Apache License 2.0"
--- src/clarango/utilities/database_utility.clj
+++
@@ -1,5 +0,0 @@
-;; Please note:
-;; The methods in this namespace are only intended for internal use.
-
-(ns clarango.utilities.database-utility
-  (:require [clarango.core :as clarango.core]))
