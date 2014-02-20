aaef895e5f0e2eadc2959e10a5488c7054a2bae3
Stefan Edlich <edlich@gmail.com>
2014-02-20 14:49:12 +0100

    get-info-user works for linux

--- test/clarango/test/database.clj
+++ test/clarango/test/database.clj
@@ -13,16 +13,13 @@
   (println "*** Database List: " (db/get-info-list))
 
   (set-default-db! testdb)
-  (println "*** Info Current <" (db/get-info-current) ">")
-  ;; even if I set another db, I get info about the _system db??
+  (println "*** Info Current <" (db/get-info-current) ">") ; yields info on _system or current?
 
-  ;; error 400 "bad parameter!!"
-;  (println "*** User Access DBs: <" (db/get-info-user) ">") 
+  ;; Windows shows: "error 400 "bad parameter"!
+  (println "*** User Access DBs: <" (db/get-info-user) ">") 
 
-  ;; Both methods get:
-  ;; Wrong number of args (4) passed to: http-utility$get-uri
-;  (println "Collections in the default DB: <" (db/get-collection-info-list) ">")
-;  (println "Collections in the testDB: <" (db/get-collection-info-list testdb) ">")
+  (println "Collections in the default DB: <" (db/get-collection-info-list) ">")
+  (println "Collections in the testDB: <" (db/get-collection-info-list testdb) ">")
 
   (db/delete testdb)
   (is (> (count (db/get-info-list)) 0) "The systen must be in the original state!") 
