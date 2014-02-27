2a8636572ae41d8b70c10a4fe19ff8460b638a74
Stefan Edlich <edlich@gmail.com>
2014-02-27 15:29:13 +0100

    Codox can now create nice docs

--- project.clj
+++ project.clj
@@ -5,4 +5,11 @@
             :url "http://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.7"]
-                 [cheshire "5.3.1"]])
+                 [cheshire "5.3.1"]]
+  :codox {:exclude [clarango.utilities.core-utility
+                    clarango.utilities.http-utility
+                    clarango.utilities.uri-utility]
+          :src-dir-uri "https://github.com/edlich/clarango/blob/master/"
+          :src-linenum-anchor-prefix "L"}
+  :plugins [[codox "0.6.7"]]
+ )
