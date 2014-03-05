690c69d5d9cb62cc1080c3e5c3b96986093dfbe8
Peter Fessel <peter.fessel@live.de>
2014-03-05 13:40:51 +0100

    continue work on new main method

--- src/clarango/main.clj
+++ src/clarango/main.clj
@@ -1,5 +1,5 @@
 (ns clarango.main
-	(:require [clarango.core :as clarango.core]
+	(:require [clarango.core :as cla-core]
       [clarango.utilities.core-utility :as core-utility]
 				[clarango.collection :as collection]
 				[clarango.document :as document]
@@ -7,8 +7,7 @@
         [clarango.query :as query]
         [clarango.graph :as graph])
 	(:use clojure.pprint)
-  (:use clarango.collection-ops)
-  (:use clarango.core))
+  (:use clarango.collection-ops))
 
 ;; DEMO to call lein run and test it without the lib usage
 (defn -main []
@@ -16,19 +15,31 @@
   ;;;; comprehensive usage example
 
   ;; connect to localhost
-  (clarango.core/set-connection!)
+  (cla-core/set-connection!)
   ;; create Database "test-DB"
   (pprint (database/create "test-DB" [{:username "test-user"}]))
   ;; create Collection "test-collection" in DB "test-DB"
   (pprint (collection/create "test-collection" "test-DB"))
-  ;; document CRUD -> create :name "some test document" :additional "some additional info" :name "new version of test document"
+  ;; document CRUD
   (pprint (document/create {:_key "test-doc" :name "some test document"} "test-collection" "test-DB"))
+  (pprint (document/update-by-key {:additional "some additional info"} "test-doc" "test-collection" "test-DB"))
   (pprint (document/get-by-key "test-doc" "test-collection" "test-DB"))
+  (pprint (document/replace-by-example {:name "new version of our test document"} {:additional "some additional info"} "test-collection" "test-DB"))
   ;; set-db "test-DB"
+  (cla-core/set-default-db! "test-DB")
   ;; collection ops : assoc, dissoc, conj
-  ;; list all documents
+  ;; get collection properties, modify properties, rename collection
+  (pprint (collection/get-info "test-collection"))
+  (pprint (collection/get-all-documents "test-collection"))
+  (pprint (collection/rename "new-name-test-collection" "test-collection"))
+  (pprint (collection/modify-properties {"waitForSync" true} "new-name-test-collection"))
+  (pprint (collection/get-extended-info-figures "new-name-test-collection"))
+  ;; unload and delete collection
+  (pprint (collection/unload "new-name-test-collection"))
+  (pprint (collection/delete "new-name-test-collection"))
 
   ;; create another Database "GraphTestDB"
+  (database/create "GraphTestDB" [])
   ;; list all databases
   ;; with-db "test-DB"
   ;; create vertex and edge collections "people", "connections"
@@ -45,6 +56,6 @@
   ;; execute traversal
   ;; delete one vertex
   ;; delete one edge
-  ;; delete collections
   ;; delete "GraphTestDB"
-  )
+  (pprint (database/delete "GraphTestDB"))
+  (database/delete "test-DB"))
