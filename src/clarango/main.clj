55c4261ca9328ff435d226e87ea77c31881c8eb9
Peter Fessel <peter.fessel@live.de>
2014-03-05 18:07:37 +0100

    continue working on new main

--- src/clarango/main.clj
+++ src/clarango/main.clj
@@ -7,7 +7,8 @@
         [clarango.query :as query]
         [clarango.graph :as graph])
 	(:use clojure.pprint)
-  (:use clarango.collection-ops))
+  (:use clarango.collection-ops)
+  (:use clarango.core))
 
 ;; DEMO to call lein run and test it without the lib usage
 (defn -main []
@@ -28,9 +29,13 @@
   ;; set-db "test-DB"
   (cla-core/set-default-db! "test-DB")
   ;; collection ops : assoc, dissoc, conj
+  (pprint (cla-assoc! "test-collection" "new-document-1" {:description "some test document to test the clojure idiomatic collection methods" :key-type "given key"}))
+  (pprint (cla-conj! "test-collection" {:description "some test document to test the clojure idiomatic collection methods" :key-type "auto generated key"}))
+  (pprint (cla-get! "test-collection" "new-document-1"))
+  (pprint (cla-dissoc! "test-collection" "new-document-1"))
   ;; get collection properties, modify properties, rename collection
-  (pprint (collection/get-info "test-collection"))
   (pprint (collection/get-all-documents "test-collection"))
+  (pprint (collection/get-info "test-collection"))
   (pprint (collection/rename "new-name-test-collection" "test-collection"))
   (pprint (collection/modify-properties {"waitForSync" true} "new-name-test-collection"))
   (pprint (collection/get-extended-info-figures "new-name-test-collection"))
@@ -41,21 +46,40 @@
   ;; create another Database "GraphTestDB"
   (database/create "GraphTestDB" [])
   ;; list all databases
+  (pprint (database/get-info-list))
   ;; with-db "test-DB"
-  ;; create vertex and edge collections "people", "connections"
-  ;; list all collections
-  ;; create graph
-  ;; get all graphs
-  ;; with-graph
-  ;; create vertices "Peter", "Bob", "Clara", "Jessica", "Alice" with :ages
-  ;; perform query: find all persons who are older than 25
-  ;; create edges with labels "friend", "boyfriend", "girlfriend"
-  ;; get vertices
-  ;; update edge :description "Peter and Alice have been friends for over 6 years."
-  ;; get edges
-  ;; execute traversal
-  ;; delete one vertex
-  ;; delete one edge
+  (with-db "GraphTestDB"
+    ;; create vertex and edge collections "people", "connections"
+    (collection/create "people" {"type" 2})
+    (collection/create "connections" {"type" 3})
+    ;; list all collections
+    (pprint (database/get-collection-info-list {"excludeSystem" true}))
+    ;; create graph
+    (pprint (graph/create "test-graph" "people" "connections"))
+    ;; get all graphs
+    (pprint (database/get-all-graphs))
+    ;; with-graph
+    (with-graph "test-graph"
+      ;; create vertices "Peter", "Bob", "Clara", "Jessica", "Alice" with :ages; save one key
+      (let [vertex-key (:_key (graph/create-vertex {:name "Peter" :age 25}))]
+        (graph/create-vertex {:name "Bob" :age 28})
+        (graph/create-vertex {:name "Clara" :age 29})
+        (graph/create-vertex {:name "Jessica" :age 23})
+        (graph/create-vertex {:name "Alice" :age 20})
+        ;; perform query: find all persons who are older than 25
+        ;; create edges with labels "friend", "boyfriend", "girlfriend"; save one key
+        (let [edge-key (graph/create-edge {:$label "friend"} )]
+        ;; get vertices
+        (pprint (graph/get-vertices vertex-key 10 10 true nil))
+        ;; update edge :description "Peter and Alice have been friends for over 6 years."
+        ;; get edges
+        (pprint (graph/get-edges edge-key 10 10 true nil))))
+      ;; execute traversal
+      ;; delete one vertex
+      ;; delete one edge
+    )
+    (pprint (graph/delete "test-graph"))
+  )
   ;; delete "GraphTestDB"
   (pprint (database/delete "GraphTestDB"))
   (database/delete "test-DB"))
