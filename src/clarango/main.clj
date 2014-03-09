1196614025ef32be4ace5f2fc91f4cf968f8c825
Peter Fessel <peter.fessel@live.de>
2014-03-09 17:50:55 +0100

    convert comments in main to printlns to make output more readable

--- src/clarango/main.clj
+++ src/clarango/main.clj
@@ -16,98 +16,98 @@
   ;;;; it tries to make use of all methods available in Clarango, but sometimes methods are left out if they are 
   ;;;; very similar to methods already used
 
-  (println "\nfirst create a database and a collection and make some document CRUD\n")
+  (println "\n\n---- first create a database and a collection and make some document CRUD ----\n")
 
-  ;; connect to defaults: localhost and port 8529
+  (println "\nconnect to defaults: localhost and port 8529")
   (cla-core/set-connection!)
-  ;; create Database "test-DB"
+  (println "\ncreate Database 'test-DB'")
   (pprint (database/create "test-DB" [{:username "test-user"}]))
-  ;; create Collection "test-collection" in DB "test-DB"
+  (println "\ncreate Collection 'test-collection' in DB 'test-DB'")
   (pprint (collection/create "test-collection" "test-DB"))
-  ;; document CRUD
+  (println "\ndocument CRUD")
   (pprint (document/create {:_key "test-doc" :name "some test document"} "test-collection" "test-DB"))
   (pprint (document/update-by-key {:additional "some additional info"} "test-doc" "test-collection" "test-DB"))
   (pprint (document/get-by-key "test-doc" "test-collection" "test-DB"))
   (pprint (document/replace-by-example {:name "new version of our test document"} {:additional "some additional info"} "test-collection" "test-DB"))
   
 
-  (println "\nnow make use of the clojure idiomatic methods available in the namespace collection-ops to add and delete more content in the collection\n")
+  (println "\n\n---- now make use of the clojure idiomatic methods available in the namespace collection-ops to add and delete more content in the collection ----\n")
 
-  ;; set default DB; this database will be used in the following methods without explicitely having to pass it
+  (println "\nset default DB; this database will be used in the following methods without explicitely having to pass it")
   (cla-core/set-default-db! "test-DB")
-  ;; collection ops : assoc, dissoc, conj
+  (println "\ncollection ops : assoc, dissoc, conj")
   (pprint (cla-assoc! "test-collection" "new-document-1" {:description "some test document to test the clojure idiomatic collection methods" :key-type "given key"}))
   (pprint (cla-conj! "test-collection" {:description "some test document to test the clojure idiomatic collection methods" :key-type "auto generated key"}))
   (pprint (cla-get! "test-collection" "new-document-1"))
   (pprint (cla-dissoc! "test-collection" "new-document-1"))
 
 
-  (println "\nmodify the collection\n")
+  (println "\n\n---- modify the collection ----\n")
 
-  ;; get information about the collection and a list of all documents inside it
+  (println "\nget information about the collection and a list of all documents inside it")
   (pprint (collection/get-info "test-collection"))
   (pprint (collection/get-all-documents "test-collection"))
-  ;; rename the collection and modify it's properties
+  (println "\nrename the collection and modify it's properties")
   (pprint (collection/rename "new-name-test-collection" "test-collection"))
   (pprint (collection/modify-properties {"waitForSync" true} "new-name-test-collection"))
   (pprint (collection/get-extended-info-figures "new-name-test-collection"))
-  ;; unload and delete collection
+  (println "\nunload and delete collection")
   (pprint (collection/unload "new-name-test-collection"))
   (pprint (collection/delete "new-name-test-collection"))
 
 
-  (println "\nnow create a graph, query it's vertices and perform some graph operations including a traversal\n")
+  (println "\n\n---- now create a graph, query it's vertices and perform some graph operations including a traversal ----\n")
 
-  ;; first create another Database "GraphTestDB"
+  (println "\nfirst create another Database 'GraphTestDB'")
   (database/create "GraphTestDB" [])
-  ;; now list all available databases
+  (println "\nnow list all available databases")
   (pprint (database/get-info-list))
-  ;; perform next operations in the context of "GraphTestDB"
+  (println "\nperform next operations in the context of 'GraphTestDB'")
   (with-db "GraphTestDB"
-    ;; create vertex and edge collections "people" and "connections"
+    (println "\ncreate vertex and edge collections 'people' and 'connections'")
     (collection/create "people" {"type" 2})
     (collection/create "connections" {"type" 3})
-    ;; now list all available collections, excluding the system collections
+    (println "\nnow list all available collections, excluding the system collections")
     (pprint (database/get-collection-info-list {"excludeSystem" true}))
-    ;; create graph "test-graph"
+    (println "\ncreate graph 'test-graph'")
     (pprint (graph/create "test-graph" "people" "connections"))
-    ;; now get all available graphs
+    (println "\nnow get all available graphs")
     (pprint (database/get-all-graphs))
-    ;; perform next operations in the context of the graph "test-graph"
+    (println "\nperform next operations in the context of the graph 'test-graph'")
     (with-graph "test-graph"
-      ;; create vertices "Peter", "Bob", "Clara", "Jessica", "Alice" with :ages
+      (println "\ncreate vertices 'Peter', 'Bob', 'Clara', 'Jessica', 'Alice' with :ages")
       (graph/create-vertex {:_key "peter" :name "Peter" :age 25})
       (graph/create-vertex {:_key "bob" :name "Bob" :age 28})
       (graph/create-vertex {:_key "clara" :name "Clara" :age 29})
       (graph/create-vertex {:_key "jessica" :name "Jessica" :age 23})
       (graph/create-vertex {:_key "alice" :name "Alice" :age 20})
 
-      (println "\nperform query: find all people who are older than 24\nfirst validate the query, then explain (how the query would be executed on the server), then actually execute it\n")
+      (println "\n\n---- perform query: find all people who are older than 24\nfirst validate the query, then explain (how the query would be executed on the server), then actually execute it ----\n")
       (pprint (query/validate "FOR p IN people FILTER p.age > 24 RETURN p"))
       (pprint (query/explain "FOR p IN people FILTER p.age > 24 RETURN p"))
       (pprint (query/execute "FOR p IN people FILTER p.age > 24 RETURN p"))
 
-      ;; create edges with labels "friend", "boyfriend", "girlfriend"; save one key to use this edge later
+      (println "\ncreate edges with labels 'friend', 'boyfriend', 'girlfriend'; save one key to use this edge later")
       (let [edge-key (get (graph/create-edge {:$label "friend"} "peter" "alice") "_key")]
         (graph/create-edge {:$label "friend"} "alice" "clara")
         (graph/create-edge {:$label "friend"} "clara" "jessica")
         (graph/create-edge {:$label "boyfriend"} "alice" "bob")
         (graph/create-edge {:$label "girlfriend"} "bob" "alice")
-        ;; get vertices that have connections going from the vertex "peter"
+        (println "\nget vertices that have connections going from the vertex 'peter'")
         (pprint (graph/get-vertices "peter" 10 10 true nil))
-        ;; update one edge
+        (println "\nupdate one edge")
         (pprint (graph/update-edge {:description "Peter and Alice have been friends for over 6 years"} edge-key))
-        ;; get all edges that are outgoing from the vertex "peter"
+        (println "\nget all edges that are outgoing from the vertex 'peter'")
         (pprint (graph/get-edges "peter" 10 10 true nil))
-        ;; execute a graph traversal
+        (println "\nexecute a graph traversal")
         (pprint (graph/execute-traversal "peter" "people" "connections" "inbound"))
-        ;; delete one edge
+        (println "\ndelete one edge")
         (pprint (graph/delete-edge edge-key))
-        ;; delete one vertex
+        (println "\ndelete one vertex")
         (pprint (graph/delete-vertex "peter"))))
-    ;; delete the graph
+    (println "\ndelete the graph")
     (pprint (graph/delete "test-graph")))
   
-  ;; delete databases
+  (println "\ndelete databases")
   (pprint (database/delete "GraphTestDB"))
   (database/delete "test-DB"))
