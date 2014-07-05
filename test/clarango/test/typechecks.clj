a73f7b006c196c79e5c761b12ff3e36880d85ab6
Peter Fessel <peter.fessel@live.de>
2014-07-05 12:16:50 +0200

    exchange 'manual' typechecks with map? and string? methods

--- test/clarango/test/typechecks.clj
+++ test/clarango/test/typechecks.clj
@@ -1,27 +1,13 @@
 (ns clarango.test.typechecks
-  (:use clojure.test clarango.core clarango.utilities.type-utility clarango.utilities.core-utility)
+  (:use clojure.test clarango.core clarango.utilities.core-utility)
   (:require [clarango.database :as db]
             [clarango.collection :as coll]
             [clarango.document :as doc]))
 
 (deftest typecheck-test
-  (testing "Testing if typechecks work: "
+  (testing "Testing if typechecks work"
 
-    #_(testing "typecheck functions on their own"
-      (is (= "test-string" (ensure-string "test-string")) "Does not let string pass")
-      (is (thrown? Exception (ensure-string {:property "test-string"})) "Must throw exception for map")
-      (is (thrown? Exception (ensure-string nil)) "Must throw exception for nil"))
-
-    #_(testing "clarango.utilities.core-utilities methods for return types"
-      (is (= clojure.lang.PersistentArrayMap (type (filter-out-map [0, 1, 2, {:name "map"}]))) "filter-out-map should only return maps")
-      (is (let [result-vec (ensure-string-vector (remove-map ["0", "1", "2", {:name "map"}]))]
-            (and 
-              (and (= (type (get result-vec 0)) java.lang.String) (= (type (get result-vec 1)) java.lang.String))
-               (= (type (get result-vec 2)) java.lang.String)))
-        "remove-map in combination with ensure-string-vector should return a vector that only contains strings")
-      (is (thrown? Exception (ensure-string-vector (remove-map ["0", nil, 2, {:name "map"}]))) "ensure-string-vector should throw an error if called on an array, that contains other types than strings"))
-
-    (testing "full example on a document function: "
+    (testing "on a document function: "
 
       ;; setup
       (set-connection!)
