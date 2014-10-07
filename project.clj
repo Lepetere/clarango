18db84bb503a70d0dcb55a7fae3d739cd2e593c0
Peter Fessel <peter.fessel@live.de>
2014-10-07 21:42:59 +0200

    update clj-http version to 1.0.0, increase clarango version number to 0.5.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clarango "0.4.2"
+(defproject clarango "0.5.0"
   :description "A Clojure client for the HTTP API of ArangoDB"
   :url "https://github.com/edlich/clarango"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.7"]
+                 [clj-http "1.0.0"]
                  [cheshire "5.3.1"]]
   :plugins [[codox "0.8.0"]]
   :codox  {:src-dir-uri "https://github.com/edlich/clarango/blob/master/"
