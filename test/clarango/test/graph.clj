81b137f834645065768f68359d235f0b4c607c7d
Peter Fessel <peter.fessel@live.de>
2015-10-23 14:57:33 +0200

    replace former graph namespace with gharial namespace and rename the latter to graph
    See discussion: https://github.com/Lepetere/clarango/issues/15

--- test/clarango/test/graph.clj
+++ test/clarango/test/graph.clj
@@ -12,94 +12,203 @@
 
 (defn setup []
   (cla-core/set-connection!)
-  (database/create "GraphTestDB" []))
+  (database/create "GharialTestDB" []))
 
 (defn teardown []
-  (pprint (database/delete "GraphTestDB")))
+  (database/delete "GharialTestDB"))
 
 (defn fixture [f]
   (setup)
   (f)
   (teardown))
 
-(use-fixtures :once fixture)
+(use-fixtures :each fixture)
 
-(deftest graph-and-query-test
+(deftest create-and-delete-single-collection-graph
 
-  (testing "get db info list"
-    (println "\nlist all available databases")
-    (pprint (database/get-info-list)))
-  
-  (testing "set db context"
-    (with-db "GraphTestDB"
-      
-      (testing "create a vertex and an edge collection and then a graph out of it"
-        (println "\ncreate vertex and edge collections 'people' and 'connections'")
-        (collection/create :people {"type" 2})
-        (collection/create :connections {"type" 3})
-        (println "\nnow list all available collections, excluding the system collections")
-        (pprint (database/get-collection-info-list {"excludeSystem" true}))
-        (println "\ncreate graph 'test-graph'")
-        (pprint (graph/create "test-graph" :people :connections))
-        (println "\nnow get all available graphs")
-        (pprint (database/get-all-graphs))
-        (println "\ntest if graph exists\n")
-        (is (database/graph-exists? "test-graph")))
+  (testing "Creating a new single-collection graph returns map of graph-information"
+    (with-db "GharialTestDB"
+      (collection/create :people {"type" 2})
+      (let [create-graph-result (graph/create :test-graph [{:edge-collection "my-test-edges" :from [:people] :to [:people]}])]
+        (is (not (nil? create-graph-result)) true)
+        (is (= "test-graph" (create-graph-result "name"))))))
 
-      (testing "set graph context and create vertices"
-        (with-graph :test-graph
-              
-          (let [bob (graph/create-vertex {:_key "bob" :name "Bob" :age 28} :people)
-                peter (graph/create-vertex {:_key "peter" :name "Peter" :age 25} :people)]
+  (testing "Delete new graph"
+    (with-db "GharialTestDB"
+      (pprint (graph/delete :test-graph)))))
 
-            (testing "create vertices"
-              (graph/create-vertex {:_key "clara" :name "Clara" :age 29} :people)
-              (graph/create-vertex {:_key "jessica" :name "Jessica" :age 23} :people)
-              (graph/create-vertex {:_key "alice" :name "Alice" :age 20} :people))
-              
-            (testing "perform query: find all people who are older than 24"
-              (pprint (query/validate "FOR p IN people FILTER p.age > 24 RETURN p"))
-              (pprint (query/explain "FOR p IN people FILTER p.age > 24 RETURN p"))
-              (pprint (query/execute "FOR p IN people FILTER p.age > 24 RETURN p")))
-              
-                    
-            (let [edge-key (get (graph/create-edge-with-key {:content "some content"} :somegreatedgekey "friend" "alice" "clara") "_key")]
-                      
-              (testing "create edges"
-                (graph/create-edge {:$label "friend" :_key "edgekey1"} peter :alice)
-                (graph/create-edge-with-key {:content "some content"} nil "friend" "clara" "jessica")
-                (graph/create-edge-with-key {:content "some content"} nil "boyfriend" :alice bob)
-                (graph/create-edge-with-key {:content "some content"} nil "girlfriend" bob :alice))
-          
-              (testing "update edge"
-                (pprint (graph/update-edge {:description "Peter and Alice have been friends for over 6 years"} edge-key)))
-                      
-              (testing "traversals" 
-                (pprint (graph/execute-vertex-traversal "peter" 10 10 true nil))
-                (println "\nget all edges that are outgoing from the vertex 'peter'")
-                (pprint (graph/execute-edge-traversal "peter" 10 10 true nil))
-                (println "\nexecute a graph traversal")
-                (pprint (graph/execute-traversal "peter" "people" "connections" "inbound")))
-                      
-              (testing "delete edge"
-                (pprint (graph/delete-edge edge-key))))))
-        
-        (testing "delete vertex" 
-          (println "\ndelete one vertex")
-          (pprint (graph/delete-vertex "peter" :test-graph)))
-        
-        (testing "delete graph" 
-          (println "\ndelete the graph")
-          (pprint (graph/delete :test-graph)))
+(deftest create-and-delete-multi-collection-graph
 
-        (testing "test nested graph/collection creation"
-          (println "test nested graph/collection creation")
-          (pprint (graph/create :test-graph-2 (collection/create :people-2 {"type" 2}) (collection/create :connections-2 {"type" 3}))))))))
+  (testing "Creating a new multi-collection graph returns a map of graph-information"
+    (with-db "GharialTestDB"
+      (collection/create :people {"type" 2})
+      (collection/create :content {"type" 2})
+      (let [create-graph-result (graph/create :test-graph [{:edge-collection "my-test-edges" :from [:people] :to [:content]}])]
+        (is (not (nil? create-graph-result)))
+        (is (= "test-graph" (create-graph-result "name"))))))
 
-(deftest misc-test
+  (testing "Delete new graph returns a map confirming removal w/ status code 200"
+    (with-db "GharialTestDB"
+      (let [delete-graph-result (graph/delete :test-graph)]
+        (is (true? (delete-graph-result "removed")))
+        (is (= 200 (delete-graph-result "code")))))))
 
-  (testing "misc functions with output"
-    (println "\nversion without details:")
-    (pprint (misc/version))
-    (println "\nversion with details:")
-    (pprint (misc/version true))))
+(deftest basic-vertex-management
+
+  (with-db "GharialTestDB"
+    (collection/create :people {"type" 2})
+    (collection/create :content {"type" 2})
+    (graph/create :test-graph [{:edge-collection "my-test-edges" :from [:people] :to [:content]}]))
+
+  (def vertex-save-record (atom nil))
+
+  (testing "Create a new Vertex in 'people' collection returns map containing _key, _rev and _id"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [vertex-result (graph/create-vertex {:name "Rich Hickey"} :people)]
+          (is (true? (contains? vertex-result "_key")))
+          (is (true? (contains? vertex-result "_rev")))
+          (is (true? (contains? vertex-result "_id")))
+          (reset! vertex-save-record vertex-result)))))
+
+  (testing "Fetching created vertex returns previously created object"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [vertex-result (graph/get-vertex (@vertex-save-record "_key") :people)]
+          (is (true? (contains? vertex-result "_key")))
+          (is (every? true? [(contains? vertex-result "name") (= (vertex-result "name") "Rich Hickey")]))))))
+
+  (testing "Replacing a created vertex returns map containing _key, _id, an updated _rev and the _oldRev value"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [vertex-result (graph/replace-vertex {:foo "bar"} (@vertex-save-record "_key")  :people)]
+          (is (every? true? [(contains? vertex-result "_key") (contains? vertex-result "_rev") (contains? vertex-result "_id")]))
+          (is (true? (= (vertex-result "_oldRev") (@vertex-save-record "_rev"))))
+          (reset! vertex-save-record vertex-result)))))
+
+  (testing "Fetching the replaced vertex returns the new model without any previous values"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [vertex-result (graph/get-vertex (@vertex-save-record "_key") :people)]
+          (is (every? true? [(contains? vertex-result "foo") (= (vertex-result "foo") "bar")]))
+          (is (false? (contains? vertex-result "name")))))))
+
+  (testing "Updating a created vertex returns map containing _key, _id, an updated _rev and the oldRev value"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [vertex-result (graph/update-vertex {:name "Rich Hickey" :foo "baz"} (@vertex-save-record "_key") :people)]
+          (is (every? true? [(contains? vertex-result "_key") (contains? vertex-result "_rev") (contains? vertex-result "_id")]))
+          (is (true? (= (vertex-result "_oldRev") (@vertex-save-record "_rev"))))
+          (reset! vertex-save-record vertex-result)))))
+
+  (testing "Fetching the updated vertex returns the updated model with new and updated fields"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [vertex-result (graph/get-vertex (@vertex-save-record "_key") :people)]
+          (is (every? true? [(contains? vertex-result "foo") (= (vertex-result "foo") "baz") ]))
+          (is (every? true? [(contains? vertex-result "name") (= (vertex-result "name") "Rich Hickey")]))))))
+
+  (testing "Deleting the vertex returns a map with 'code' => '202' and 'removed' => true"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [delete-result (graph/delete-vertex (@vertex-save-record "_key") :people)]
+          (is (every? true? [(contains? delete-result "code") (= (delete-result "code") 202)]))
+          (is (every? true? [(contains? delete-result "removed") (= (delete-result "removed") true)])))))))
+
+(deftest basic-edge-management
+
+  ; Initialise
+  (with-db "GharialTestDB"
+    (collection/create :people {"type" 2})
+    (collection/create :content {"type" 2})
+    (graph/create :test-graph [{:edge-collection "my-test-edges" :from [:people] :to [:content]}]))
+
+  (def edge-save-result (atom nil))
+
+  (testing "Adding a new edge in 'my-test-edges' collection returns map containing _key, _rev and _id"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [edge-result (graph/create-edge {:type "foo"} (graph/create-vertex {:name "bar"} :people) (graph/create-vertex {:title "stuff"} :content) :my-test-edges)]
+          (is (every? true? [(contains? edge-result "_key") (contains? edge-result "_rev") (contains? edge-result "_id")]))
+          (reset! edge-save-result edge-result)))))
+
+  (testing "Fetching created edge returns previously created object"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [edge-result (graph/get-edge (@edge-save-result "_key") :my-test-edges)]
+          (is (every? true? [(contains? edge-result "_key") (contains? edge-result "type")]))
+          (is (true? (= (edge-result "type") "foo")))))))
+
+  (testing "Replacing a created edge returns map containing _key, _rev, an updated _rev and the _oldRev value"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [edge-result (graph/replace-edge {:foo "bar"} (@edge-save-result "_key") :my-test-edges)]
+          (is (every? true? [(contains? edge-result "_key") (contains? edge-result "_rev") (contains? edge-result "_id") (contains? edge-result "_oldRev")]))
+          (is (true? (= (edge-result "_oldRev") (@edge-save-result "_rev"))))
+          (reset! edge-save-result edge-result)))))
+
+  (testing "Fetching the replaced edge returns the new model without any previous values"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [edge-result (graph/get-edge (@edge-save-result "_key") :my-test-edges)]
+          (is (true? (contains? edge-result "foo")))
+          (is (true? (= (edge-result "foo") "bar")))
+          (is (false? (contains? edge-result "type")))))))
+
+  (testing "updating an existing edge returns a map containing _key, _id, an updated _rev and the old_rev"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [edge-result (graph/update-edge {:type "my-type"} (@edge-save-result "_key") :my-test-edges)]
+          (is (every? true? [(contains? edge-result "_key") (contains? edge-result "_rev") (contains? edge-result "_id")]))
+          (is (true? (= (edge-result "_oldRev") (@edge-save-result "_rev"))))
+          (reset! edge-save-result edge-result)))))
+
+  (testing "Fetching the updated vertex returns the updated model with both previous and updated fields"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [edge-result (graph/get-edge (@edge-save-result "_key") :my-test-edges)]
+          (is (every? true? [(contains? edge-result "foo") (= (edge-result "foo") "bar")]))
+          (is (every? true? [(contains? edge-result "type") (= (edge-result "type") "my-type")]))))))
+
+  (testing "Deleting the edge returns a map with 'code' => '202' and 'removed' => true"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (let [delete-result (graph/delete-vertex (@edge-save-result "_key") :my-test-edges)]
+          (is (every? true? [(contains? delete-result "code") (= (delete-result "code") 202)]))
+          (is (every? true? [(contains? delete-result "removed") (= (delete-result "removed") true)])))))))
+
+(deftest graph-and-query-and-traversal-test
+
+  (testing "Initialising Test Graph"
+    (with-db "GharialTestDB"
+      (collection/create :people {"type" 2})
+      (collection/create :content {"type" 2})
+      (graph/create "test-graph" [{:edge-collection :actions :from [:people] :to [:content]}])
+      (is (true? (database/graph-exists? "test-graph")))))
+
+  (testing "Initialising Test Graph Vertices Data"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (graph/create-vertex-with-key {:name "Bob" :age 28} "bob-key" :people)
+        (graph/create-vertex-with-key {:name "Peter" :age 25} "peter-key" :people)
+        (graph/create-vertex-with-key {:title "Some great content"} "great-content-key" :content)
+        (graph/create-vertex-with-key {:title "Some rather 'meh' content"} "meh-content-key" :content))))
+
+  (testing "Initialising Test Graph Edges Data"
+    (with-db "GharialTestDB"
+      (with-graph "test-graph"
+        (graph/create-edge {:action_type "viewed"} (graph/get-vertex "bob-key" :people) (graph/get-vertex "great-content-key" :content) :actions)
+        (graph/create-edge {:action_type "viewed"} (graph/get-vertex "peter-key" :people) (graph/get-vertex "great-content-key" :content) :actions)
+        (graph/create-edge {:action_type "liked"} (graph/get-vertex "bob-key" :people) (graph/get-vertex "meh-content-key" :content) :actions))))
+
+  (testing "Outbound traversal from Peter returns:
+            - a map
+            - contains a 'vertices' and a 'path' element
+            - vertices stanza has entries for 'Peter' (source) and 'Some great content' (target)"
+    (with-db "GharialTestDB"
+      (with-graph "test-db"
+        (let [traversal-result (graph/execute-traversal "peter-key" "people" "actions" "outbound")]
+          (is (map? traversal-result))
+          (is (every? true? [(contains? traversal-result "vertices") (contains? traversal-result "paths")]))
+          (is (every? true? [(contains? (first (traversal-result "vertices")) "name") (= (get (first (traversal-result "vertices")) "name") "Peter")]))
+          (is (every? true? [(contains? (second (traversal-result "vertices")) "title") (= (get (second (traversal-result "vertices")) "title") "Some great content")])))))))
